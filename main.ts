83a9c3e17b1ec7ddeedf06dbefe25762ef93496e
randomguy048 <jkmaster236@gmail.com>
2026-01-28 10:16:27 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,13 +9,19 @@ basic.showLeds(`
     `)
 music.play(music.stringPlayable("- - - - - - C D ", 70), music.PlaybackMode.UntilDone)
 music.play(music.stringPlayable("B A G F E D C C5 ", 550), music.PlaybackMode.UntilDone)
-music.play(music.stringPlayable("C5 - - - - - - - ", 70), music.PlaybackMode.UntilDone)
+music.play(music.tonePlayable(523, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
 basic.clearScreen()
 basic.forever(function () {
-    basic.pause(1800000)
+    basic.pause(5000)
     while (0 < hours_to_remind) {
-        music.play(music.stringPlayable("C D G F C D G F ", 600), music.PlaybackMode.LoopingInBackground)
-        basic.showNumber(4.5)
+        music.play(music.stringPlayable("C - C - C - E - ", 600), music.PlaybackMode.InBackground)
+        basic.showLeds(`
+            # . . . #
+            # . . . #
+            # # # # #
+            # . . . #
+            . # # # .
+            `)
         if (input.buttonIsPressed(Button.A)) {
             hours_to_remind += -0.5
             music.stopMelody(MelodyStopOptions.All)
@@ -26,11 +32,12 @@ basic.forever(function () {
                 . . . . .
                 . . . . .
                 `)
-            music.play(music.stringPlayable("C - C5 - - - - - ", 500), music.PlaybackMode.UntilDone)
+            music.play(music.stringPlayable("C F G C F G B C5 ", 500), music.PlaybackMode.UntilDone)
             basic.clearScreen()
-            basic.pause(1800000)
+            basic.pause(5000)
             continue;
         }
+        basic.pause(420)
     }
     if (minutes_to_sleep == 510) {
         while (minutes_to_sleep > 0) {
@@ -41,7 +48,7 @@ basic.forever(function () {
                 . . . . .
                 . . . . .
                 `)
-            basic.pause(1800000)
+            basic.pause(5000)
             minutes_to_sleep += -30
         }
     } else {
